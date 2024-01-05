b129e012f4897cd948808cdb24aa89f7d5c2d0f4
Torsten Landsiedel <torsten.landsiedel@gmx.de>
2024-01-05 19:51:50 +0100

    Add year to copyright notice

--- aged-content-message.php
+++ aged-content-message.php
@@ -13,7 +13,7 @@
 
 /*
 Copyright (C)  2014-2023 Caspar Hübinger
-Copyright (C)  2023- Torsten Landsiedel
+Copyright (C)  2023-2024 Torsten Landsiedel
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
